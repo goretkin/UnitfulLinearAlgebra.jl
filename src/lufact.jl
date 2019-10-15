68b67e65f19eff4349c67345bae8827f3d522892
Gustavo Goretkin <gustavo.goretkin@gmail.com>
2019-10-15 16:55:51 -0400

    Copy Julia's generic_lufact to begin Units work

---
+++ src/lufact.jl
@@ -0,0 +1,51 @@
+using LinearAlgebra: BlasInt, checknonsingular, LU
+
+function my_generic_lufact!(A::StridedMatrix{T}, ::Val{Pivot} = Val(true);
+                         check::Bool = true) where {T,Pivot}
+    m, n = size(A)
+    minmn = min(m,n)
+    info = 0
+    ipiv = Vector{BlasInt}(undef, minmn)
+    @inbounds begin
+        for k = 1:minmn
+            # find index max
+            kp = k
+            if Pivot
+                amax = abs(zero(T))
+                for i = k:m
+                    absi = abs(A[i,k])
+                    if absi > amax
+                        kp = i
+                        amax = absi
+                    end
+                end
+            end
+            ipiv[k] = kp
+            if !iszero(A[kp,k])
+                if k != kp
+                    # Interchange
+                    for i = 1:n
+                        tmp = A[k,i]
+                        A[k,i] = A[kp,i]
+                        A[kp,i] = tmp
+                    end
+                end
+                # Scale first column
+                Akkinv = inv(A[k,k])
+                for i = k+1:m
+                    A[i,k] *= Akkinv
+                end
+            elseif info == 0
+                info = k
+            end
+            # Update the rest
+            for j = k+1:n
+                for i = k+1:m
+                    A[i,j] -= A[i,k]*A[k,j]
+                end
+            end
+        end
+    end
+    check && checknonsingular(info)
+    return LU{T,typeof(A)}(A, ipiv, convert(BlasInt, info))
+end
