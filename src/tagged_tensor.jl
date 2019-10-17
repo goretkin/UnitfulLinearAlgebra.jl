84c239e870704e37a0efffe11a3391d8fd023e71
Gustavo Goretkin <gustavo.goretkin@gmail.com>
2019-10-17 17:14:48 -0400

    Use lifting to define new ops

--- src/tagged_tensor.jl
+++ src/tagged_tensor.jl
@@ -11,7 +11,15 @@ function TTensor(tag, x)
   TaggedTensor{T, ndims(x), typeof(x), tag}(x)
 end
 
+
 get_tag(::TaggedTensor{T, N, A, TAG}) where {T, N, A, TAG} = TAG
+get_wrapped(m::TaggedTensor) = m.x
+get_tag(q::Number) = Unitful.unit(q)
+get_wrapped(q::Number) = Unitful.ustrip(q)
+# TODO ???
+get_tag(q::FreeUnits) = q
+get_wrapped(q::FreeUnits) = true
+
 get_Tuple(::TagsOuterProduct{TUPLE}) where {TUPLE} = TUPLE
 
 Tuple_to_tuple(::Type{TUPLE}) where {TUPLE<:Tuple} = tuple(TUPLE.parameters...)
@@ -22,11 +30,54 @@ Base.setindex!(t::TaggedTensor, val, ind...) = setindex!(t.x, val / get_tag(t)[i
 #get_freeunits(quantity::Unitful.Quantity{T, Dim, Unit}) = Unit
 
 
-function Base.:*(t1::TaggedTensor, t2::TaggedTensor)
-  TAG1 = get_tag(t1)
-  TAG2 = get_tag(t2)
-  TAG = TAG1 * TAG2
-  t = t1.x * t2.x
-
-  return TTensor(TAG, t)
+# TODO rewrite as macro, unless constant propagation on `operation` and splatting is free.
+function lower_operation(operation, ts...)
+  tags = map(get_tag, ts)
+  tag = operation(tags...)
+  arrs = map(get_wrapped, ts)
+  t = operation(arrs...)
+  return TTensor(tag, t)
 end
+
+
+Multipliable = Union{Number, FreeUnits}
+Base.:*(a::TaggedTensor, b::TaggedTensor) = lower_operation(*, a, b)
+Base.:*(a::TaggedTensor, b::Multipliable) = lower_operation(*, a, b)
+Base.:*(a::Multipliable, b::TaggedTensor) = lower_operation(*, a, b)
+# for the sake of avoiding method ambiguity errors. not sure how to use Multipliable otherwise
+Base.:*(a::TaggedTensor, b::Number) = lower_operation(*, a, b)
+Base.:*(a::Number, b::TaggedTensor) = lower_operation(*, a, b)
+
+const TaggedTensor2 = TaggedTensor{T, 2} where T
+
+Base.one(t::TaggedTensor) = lower_operation(one, t)
+
+constant_like(top, k) = TTensor(top, fill(k, size(top)))
+Base.zero(t::TaggedTensor) = TTensor(get_tag(t), zero(get_wrapped(t)))
+
+# TODO rewrite as macro, unless constant propagation on `operation` and splatting is free.
+function lower_operation_endomorphic(operation, t)
+  tag = canonicalize(get_tag(t))
+  @assert is_endomorphic(tag)
+  return TTensor(tag, operation(get_wrapped(t)))
+end
+
+Base.exp(t::TaggedTensor) = lower_operation_endomorphic(exp, t)
+Base.sqrt(t::TaggedTensor) = lower_operation_endomorphic(exp, t)
+
+# TODO maybe extend `lower_operation_endomorphic` to obviate this
+power(t::TaggedTensor, p) = TTensor(get_tag(t)^p, get_wrapped(t)^p)
+# method ambiguities
+Base.:^(t::TaggedTensor2, p::Integer) = power(t, p)
+Base.:^(t::TaggedTensor2, p::Real) = power(t, p)
+
+Base.literal_pow(::typeof(^), t::TaggedTensor2, ::Val{p}) where p = power(t, p)
+
+#canonicalize(t::TaggedTensor) = TTensor(canonicalize(get_tag(t)), t)
+
+# This method definition in Base messes things up!
+#isapprox(x::AbstractArray{S}, y::AbstractArray{T};
+#    kwargs...) where {S <: AbstractQuantity,T <: AbstractQuantity} = false
+
+
+
--- test/runtests.jl
+++ test/runtests.jl
@@ -128,6 +128,8 @@ Identity_A = TTensor(top3,
   ]
 )
 
+isapproxkw(a, b, atol) = isapprox(a, b, atol=atol)
+
 @testset "linear algebra" begin
   @test one(t2) * t2 == t2
   @test t2 * one(t2) == t2
@@ -135,7 +137,8 @@ Identity_A = TTensor(top3,
 
   @test canonicalize(top3) === canonicalize(top2 * u"s")
   @test LinearAlgebra.det(SHO_A) == +1.0*u"s^-2"
-  @test exp(SHO_A * 2π*u"s") ≈ Identity_A
+  @test all(isapproxkw.(exp(SHO_A * 2π*u"s"), Identity_A, ULA.constant_like(get_tag(Identity_A), 1e-15)))
+  #@test all(isapproxkw.(exp(SHO_A * (2π/60*u"minute")), Identity_A, constant_like(get_tag(Identity_A), 1e-15)))
 end
 
 end
