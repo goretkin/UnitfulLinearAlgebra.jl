cddd3cd8d472121a615cf5994f8a3621f5677b30
Gustavo Goretkin <gustavo.goretkin@gmail.com>
2020-12-19 00:14:59 -0500

    Get some tests to pass with type piracy

--- src/unitful.jl
+++ src/unitful.jl
@@ -4,10 +4,13 @@ using LinearAlgebra: LinearAlgebra, dot
 # then generic dot product fallback should work.
 # figure out what mathematical object this would imply
 # (structure is different from addition. no zero, no negative (no identity, no inverse))
-# Base.:+(a::FreeUnits, b::FreeUnits) = unit(true * a + true * b)
+Base.:+(a::FreeUnits, b::FreeUnits) = unit(true * a + true * b)
 
 Base.adjoint(x::FreeUnits) = x
 Base.transpose(x::FreeUnits) = x
 
 # TODO: alternatively, define `iterate` for `FreeUnits`, just like it is for `Number` This would be consistent with Julia's design.
-LinearAlgebra.dot(a::FreeUnits, b::FreeUnits) = a * b
+# this worked to get the correct behavior for
+# *(::LinearAlgebra.Adjoint{Unitful.FreeUnits,TagsAlongAxis{...}}, ::TagsAlongAxis{...})
+# before https://github.com/JuliaLang/julia/pull/36679
+# LinearAlgebra.dot(a::FreeUnits, b::FreeUnits) = a * b
